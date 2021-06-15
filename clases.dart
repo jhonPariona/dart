de5cdefaa96b34aaee172a52c8967477436818b7
jhonPariona <62570198+jhonPariona@users.noreply.github.com>
2021-06-15 17:10:42 -0500

    📚 UPDATE static methods

--- clases.dart
+++ clases.dart
@@ -1,7 +1,10 @@
 // Antes de nullsafety
 class Persona {
+  //variables de instancia
   String name, surname;
   int age;
+  //tbn se puede crear metodos estaticos
+  static String siempre = 'Se mantiene aunque instanciemos';
 
   //constructor
   /*
@@ -15,7 +18,7 @@ class Persona {
   // sintax sugar de constructor
   Persona(this.name, this.surname, this.age);
   
-  // constructor
+  // constructor nombrado
   Persona.withoutAge(String name, String surname) {
     this.name = name;
     this.surname = surname;
@@ -26,9 +29,33 @@ class Persona {
   bool isMayor(Persona p){
     return this.age > p.age;
   }
+  
+  // setter y getters
+  set cambiarNombre(String name){
+    this.name = name;
+  }
+  
+  String get getName{
+   return this.name; 
+  }
+  
+  //métodos staticos
+  // solo se pueden llamar variables de instancia estaticas o sino se debe crear una instancia para poder acceder
+  static metodoStatic(){
+    siempre = 'cambian una variable statica';
+    
+    //para acceder a una propiedad no estatica se debe
+    Persona p = Persona('Jh', 'PV', 23);
+    p.name = 'cambiado';
+  }
+  
+  
 }
 
+
+
 void main(List<String> args) {
+  //se puede omitir la palabra new
   Persona p = new Persona('Jhon', 'Pariona', 23);
   p.name = 'Otro';
   print(p.name);/*Otro*/
@@ -36,6 +63,18 @@ void main(List<String> args) {
   Persona p2 = new Persona.withoutAge('Kevin', 'Pariona');
   print(p2.age);/*0*/
   
+  print(Persona.siempre); /*Se mantiene aunque instanciemos -> se llama solo con el nombre la clase*/
+  
   bool isMayor = p.isMayor(p2);
   print(isMayor);/*true*/
+  
+  // getters y setter
+  Persona pgs = Persona('Jhon', 'Pariona', 22);
+  print(pgs.getName);/*Jhon*/
+  pgs.cambiarNombre('Kevin');
+  print(pgs.getName);/*Kevin*/
+  
+  //static se puede llamar con el nombre de la clase pero no desde un objeto
+  print(Persona.siempre); /*Se mantiene aunque instanciemos*/
+
 }
