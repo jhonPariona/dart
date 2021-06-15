0d60036f7a0cd19789b944d2e839b28366b88885
jhonPariona <62570198+jhonPariona@users.noreply.github.com>
2021-06-15 09:18:19 -0500

    🔶 UPDATE set addAll

--- Set.dart
+++ Set.dart
@@ -1,7 +1,16 @@
 // solo acepta valores únicos
-
+// tbn se conocen como conjuntos
 void main() {  
   Set <String> s = {"asno", "burro", "asno"};
   print(s.runtimeType); /*_LinkedHashSet<String>*/
   print(s); /*{asno, burro}*/
+  
+  // agregar un valor
+  s.add('caballo');
+  print(s); //{asno, burro, caballo}
+  
+  //agregar otro conjunto
+  Set<String> sa = {'mono', 'orangutan'};
+  s.addAll(sa);
+  print(s);//{asno, burro, caballo, mono, orangutan}
 }
